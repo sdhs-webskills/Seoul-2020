12968f7dfb8fb011a742ce7e79e25823c2e86a92
junilhwang <tjsdlf4261@naver.com>
2020-08-24 17:22:08 +0900

    refactor: app.js 리팩토링 완료

--- js/app.js
+++ js/app.js
@@ -3,37 +3,53 @@ import $ from './jquery-3.4.1.min.js';
 import Templates from "./Templates.js";
 
 class App {
-	#data; #renderer; #cover;
+	#state; #target;
 
-	constructor(data, renderer) {
-		this.#data = data;
-		this.#renderer = renderer;
-		this.#cover = $(".contents .cover");
-		this.#data.forEach(data => this.init(data));
-		this.event();
+	constructor (papers) {
+		this.#state = {
+			papers: papers.map(property => ({
+				...property,
+				get src () {
+					return `/imgs/papers/${property.image}`;
+				}
+			}))
+		}
+		this.#target = $(".contents .cover");
+		this.#render();
+		this.#event();
 	}
 
-	init ({ image, ...property }) {
-		const template = this.#renderer({
-			...property,
-			src: `/imgs/papers/${image}`
-		});
-		console.log(template);
-		this.#cover.append(template);
+	#render () {
+		this.#target.html(
+			this.#state.papers.map(({ id, src, paper_name, company_name, width_size, height_size, point, hash_tags }) => `
+				<div class="content flex center rel" data-id ="${id}">
+					<div class="img-box rel"><img src="${src}" alt="${src}" title="${src}"></div>
+					<div class="box rel">
+						<p><b>이름:</b> <span class="name">${paper_name}</span></p>
+						<p><b>업체명:</b> <span>${company_name}</span></p>
+						<p><b>가로/세로 사이즈:</b> <br> <span>${width_size} / ${height_size}</span></p>
+						<p><b>포인트:</b> <span>${point}</span></p>
+						<p><b>해시태그:</b> ${hash_tags.map(v => `<span class="hash">${v}</span>`)}</p>
+						<button type="button" class="buyBtn">구매하기</button>
+					</div>
+				</div>
+			`)
+		)
 	}
 
-	buyBtn (e) {
-		let tg = $(e.target);
-		console.log(tg);
+	#event () {
+		$('.buyBtn', this.#target)
+			.on('click', e => {
+				const index = $(e.target).parents('.content').index();
+				console.log(this.#state.papers[index]);
+			})
 	}
-
-	event () {}
 }
 
 window.onload = async () => {
-	const data = await fetch("/json/papers.json").then(response => response.json());
-	const hashData = [ ...new Set(data.flatMap(v => v.hash_tags)) ];
+	const papers = await fetch("/json/papers.json").then(response => response.json());
+	const hashData = [ ...new Set(papers.flatMap(v => v.hash_tags)) ];
 	const templates = new Templates();
-	window.hash = new Hash('.online_store', data, hashData, templates.renderMainList);
-	window.app = new App(data, templates.renderHashTags);
+	window.hash = new Hash('.online_store', papers, hashData, templates.renderHashTags);
+	window.app = new App(papers);
 }
