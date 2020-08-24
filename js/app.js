7074080781e254b33e3fad1c2996d1c230afa77d
junilhwang <tjsdlf4261@naver.com>
2020-08-24 18:06:04 +0900

    refactor: PapersService 추가

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
-import { Hash, Papers } from "./components/index.js";
-import Templates from "./Templates.js";
+import { Search, Papers } from "./components/index.js";
 
 window.onload = async () => {
-	const papersData = await fetch("/json/papers.json").then(response => response.json());
-	const hashData = [ ...new Set(papersData.flatMap(v => v.hash_tags)) ];
-	const templates = new Templates();
-	window.hash = new Hash('.online_store', papersData, hashData, templates.renderHashTags);
-	window.app = new Papers(papersData);
+	new Papers();
+	new Search();
 }
---
+++ js/services/PapersService.js
@@ -0,0 +1,20 @@
+import { HashService } from "./HashService.js";
+
+export const PapersService = {
+  papers: [],
+
+  async load () {
+    const papers = await fetch('/json/papers.json').then(response => response.json());
+    HashService.load(papers);
+    this.set(papers);
+    return papers;
+  },
+
+  set (papers) {
+    this.papers = papers;
+  },
+
+  get () {
+    return this.papers;
+  },
+}
